f367dd0c9106298fd71f22c843e7ed1c50680353
Michael Collins <michaelmartincollins@gmail.com>
2019-11-05 16:09:54 -0800

    Added detection for camera, coin prefabs, interaction interface

---
+++ Assets/Scripts/Coin.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Coin : MonoBehaviour, I_Interactable
+{
+    public void OnInteract()
+    {
+        Collected();
+    }
+
+    private void Collected()
+    {
+        print("Coin collected.");
+    }
+}
---
+++ Assets/Scripts/Rotate.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotate : MonoBehaviour
+{
+    public float xRot, yRot, zRot;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(xRot, yRot, zRot);
+    }
+}
