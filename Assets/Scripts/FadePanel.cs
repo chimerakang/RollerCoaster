91990af421c63b27f2cc858c1f6738a36a47a691
Michael Collins <MichaelMartinCollins@gmail.com>
2019-11-07 14:37:25 -0800

    Added more buildings, added tunnels, tunnel shadows.

---
+++ Assets/Scripts/FadePanel.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+using UnityEngine.UI;
+
+public class FadePanel : MonoBehaviour
+{
+    private void Start()
+    {
+        GetComponent<Image>().DOFade(0, 4.5f);
+    }
+}
---
+++ Assets/Scripts/RandomCarColor.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomCarColor : MonoBehaviour
+{
+    private void Start()
+    {
+        foreach (Renderer r in GetComponentsInChildren<Renderer>())
+        {
+            r.material.color = Random.ColorHSV();
+        }
+    }
+}
