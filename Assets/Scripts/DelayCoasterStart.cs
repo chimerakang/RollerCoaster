b43107a4f6dc9a30702db5c676c75bb1d9644c16
Michael Collins <michaelmartincollins@gmail.com>
2019-11-07 17:13:07 -0800

    Added more detail and track

---
+++ Assets/Scripts/DelayCoasterStart.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using PathCreation.Examples;
+
+public class DelayCoasterStart : MonoBehaviour
+{
+    public int delay = 10;
+
+    private void Start()
+    {
+        StartCoroutine(DelayStart());
+
+    }
+    public IEnumerator DelayStart()
+    {
+        float speed = GetComponent<PathFollower>().speed;
+        GetComponent<PathFollower>().speed = 0;
+        yield return new WaitForSeconds(delay);
+        GetComponent<PathFollower>().speed = speed;
+    }
+}
